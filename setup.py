20330a8985be641d13c000fd84cc5e39fa62bd61
Mark Lalor <markwlalor@gmail.com>
2018-02-11 01:58:20 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Clanvas',
+    version='0.1.0',
+    url='https://github.com/MarkLalor/Clanvas',
+    license='LICENSE',
+    author='Mark Lalor',
+    author_email='markwlalor@gmail.com',
+    install_requires=['canvasapi'],
+    description='Command-line client for Canvas by Instructure '
+)
