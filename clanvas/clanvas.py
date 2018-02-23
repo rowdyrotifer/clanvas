ce6edb5d1b22b3f615771f74ab7827b5b429b7eb
Mark Lalor <markwlalor@gmail.com>
2018-02-23 03:39:03 -0500

    Add cached decorators and optional course param for course-based commands.

--- clanvas/clanvas.py
+++ clanvas/clanvas.py
@@ -15,35 +15,20 @@ from canvasapi.course import Course
 from colorama import Fore, Style
 from tabulate import tabulate
 from tree_format import format_tree
-from tzlocal import get_localzone
 
-local_tz = get_localzone()
-
-complete_cd = functools.partialmethod(cmd2.Cmd.path_complete, dir_only=True)
-complete_cat = functools.partialmethod(cmd2.Cmd.path_complete, dir_only=False)
-
-# For specifying tab-completion for default shell commands
-# TODO: add basically everything from GNU Coreutils http://www.gnu.org/software/coreutils/manual/html_node/index.html
-completion_map_dir_only = ['cd']
-completion_map_dir_file = ['cat', 'tac', 'nl', 'od', 'base32', 'base64', 'fmt', 'tail', 'ls']
-
-# Extend the course object to have a readable-yet-unique code that is course code + id
-Course.unique_course_code = property(lambda self: self.course_code.replace(' ', '') + '-' + str(self.id))
-
-def tabulate_dict(item_to_list, items):
-    tabulate_list = tabulate(map(item_to_list, items), tablefmt='plain').split('\n')
-    result = {}
-    i = 0
-    for item in items:
-        result[item] = tabulate_list[i]
-        i += 1
-    return result
+import utils
+from utils import cached_invalidatable
 
 
 class Clanvas(cmd2.Cmd):
     default_to_shell = True
 
     def __init__(self):
+        # For specifying tab-completion for default shell commands
+        # TODO: add basically everything from GNU Coreutils http://www.gnu.org/software/coreutils/manual/html_node/index.html
+        completion_map_dir_only = ['cd']
+        completion_map_dir_file = ['cat', 'tac', 'nl', 'od', 'base32', 'base64', 'fmt', 'tail', 'ls']
+
         for command in completion_map_dir_only:
             setattr(Clanvas, 'complete_' + command, functools.partialmethod(cmd2.Cmd.path_complete, dir_only=True))
         for command in completion_map_dir_file:
@@ -62,41 +47,14 @@ class Clanvas(cmd2.Cmd):
         self.current_course = None  # type: Course
         self.current_directory = self.home
 
-        self.__courses = None
-        self.__current_user_profile = None
+    @cached_invalidatable
+    def get_courses(self, **kwargs):
+        return sorted(self.canvas.get_current_user().get_courses(),
+                      key=lambda course: (-course.enrollment_term_id, course.name))
 
-    def get_courses(self, invalidate=False):
-        if self.__courses is None or invalidate:
-            sorter = lambda course: (-course.enrollment_term_id, course.name)
-            self.__courses = sorted(self.canvas.get_current_user().get_courses(), key=sorter)
-
-        return self.__courses
-
-    def current_user_profile(self, invalidate=False):
-        if self.__current_user_profile is None or invalidate:
-            self.__current_user_profile = self.canvas.get_current_user().get_profile()
-
-        return self.__current_user_profile
-
-    @staticmethod
-    def course_info_items(c):
-        return [c.course_code, c.id, c.start_at_date.strftime("%b %y") if hasattr(c, 'start_at_date') else '', c.name]
-
-    @staticmethod
-    def course_query_items(c):
-        return [c.course_code, c.id, c.unique_course_code, c.name]
-
-    @staticmethod
-    def assignment_info_items(a):
-        return [a.id, a.due_at_date.astimezone(local_tz).strftime("%a, %d %b %I:%M%p") if hasattr(a, 'due_at_date') else '', a.name]
-
-    @staticmethod
-    def submission_info_items(s):
-        return [s.id, s.score if hasattr(s, 'score') else '']
-
-    @staticmethod
-    def query_courses(courses, query):
-        return filter(lambda course: any([query in str(item) for item in Clanvas.course_query_items(course)]), courses)
+    @cached_invalidatable
+    def current_user_profile(self, **kwargs):
+        return self.canvas.get_current_user().get_profile()
 
     # cmd2 attribute, made dynamic with get_prompt()
     prompt = property(lambda self: self.get_prompt())
@@ -104,16 +62,16 @@ class Clanvas(cmd2.Cmd):
     prompt_string = Fore.LIGHTGREEN_EX + '{login_id}@{host}' + Style.RESET_ALL + ':' + Fore.YELLOW + '{pwc}' + Style.RESET_ALL + ':' + Fore.BLUE + '{pwd} ' + Style.RESET_ALL + '$ '
 
     def get_prompt(self):
+
         if self.canvas is None:
             return '$ '
 
         return self.prompt_string.format(
-            login_id=(self.current_user_profile()['login_id']),
-            host=(self.host),
-            pwc=(self.current_course.course_code if self.current_course is not None else '~'),
-            pwd=(os.getcwd().replace(self.home, '~'))
+            login_id=self.current_user_profile()['login_id'],
+            host=self.host,
+            pwc=self.current_course.course_code if self.current_course is not None else '~',
+            pwd=os.getcwd().replace(self.home, '~')
         )
-
 
     #     _____                                          _
     #    / ____|                                        | |
@@ -151,15 +109,14 @@ class Clanvas(cmd2.Cmd):
     la_parser.add_argument('-s', '--submissions', action='store_true', help='show submissions')
     la_parser.add_argument('-u', '--upcoming', action='store_true', help='show only upcoming assignments')
 
-    @cmd2.with_argparser(la_parser)
+    @cmd2.with_argparser(utils.argparser_course_optional(la_parser))
+    @utils.argparser_course_optional_wrapper
     def do_la(self, opts):
-        if self.current_course is None:
-            self.poutput('Please select a course')
+        if opts.course is None:
+            self.poutput('No course specified.')
             return False
 
-        course = self.current_course
-
-        display_assignments = course.get_assignments()
+        display_assignments = opts.course.get_assignments()
 
         if opts.upcoming:
             now = pytz.UTC.localize(datetime.now())
@@ -168,21 +125,21 @@ class Clanvas(cmd2.Cmd):
         if opts.long:
             if opts.submissions:
                 assignment_ids = map(lambda assignment: assignment.id, display_assignments)
-                assignment_submissions = course.list_multiple_submissions(assignment_ids=assignment_ids)
+                assignment_submissions = opts.course.list_multiple_submissions(assignment_ids=assignment_ids)
 
                 submissions_by_assignment = defaultdict(list)
 
-                tabulated_submissions = tabulate_dict(Clanvas.submission_info_items, assignment_submissions)
+                tabulated_submissions = utils.tabulate_dict(utils.submission_info_items, assignment_submissions)
                 for submission, formatted in tabulated_submissions.items():
                     submissions_by_assignment[submission.assignment_id].append((formatted, []))
 
-                tabulated_assignments = tabulate_dict(Clanvas.assignment_info_items, display_assignments)
+                tabulated_assignments = utils.tabulate_dict(utils.assignment_info_items, display_assignments)
 
-                tree = (course.unique_course_code, [(formatted, submissions_by_assignment[assignment.id]) for assignment, formatted in tabulated_assignments.items()])
+                tree = (utils.unique_course_code(opts.course), [(formatted, submissions_by_assignment[assignment.id]) for assignment, formatted in tabulated_assignments.items()])
 
                 self.poutput(format_tree(tree, format_node=itemgetter(0), get_children=itemgetter(1)))
             else:
-                self.poutput(tabulate(map(Clanvas.assignment_info_items, display_assignments), tablefmt='plain'))
+                self.poutput(tabulate(map(utils.assignment_info_items, display_assignments), tablefmt='plain'))
         else:
             self.poutput('\n'.join([assignment.name for assignment in display_assignments]))
 
@@ -211,7 +168,7 @@ class Clanvas(cmd2.Cmd):
 
     @cmd2.with_argparser(lc_parser)
     def do_lc(self, opts):
-        courses = self.get_courses(opts.invalidate)
+        courses = self.get_courses(invalidate=opts.invalidate)
 
         if opts.all:
             display_courses = courses
@@ -220,9 +177,9 @@ class Clanvas(cmd2.Cmd):
             display_courses = filter(lambda course: course.enrollment_term_id == latest_term, courses)
 
         if opts.long:
-            self.poutput(tabulate(map(Clanvas.course_info_items, display_courses), tablefmt='plain'))
+            self.poutput(tabulate(map(utils.course_info_items, display_courses), tablefmt='plain'))
         else:
-            self.poutput('\n'.join([c.unique_course_code for c in display_courses]))
+            self.poutput('\n'.join([utils.unique_course_code(c) for c in display_courses]))
 
     cc_parser = argparse.ArgumentParser()
     cc_parser.add_argument('course', nargs='?', default='', help='course id or matching course string (e.g. the course code)')
@@ -234,41 +191,16 @@ class Clanvas(cmd2.Cmd):
             return False
 
         courses = self.get_courses()
-        matched_courses = list(Clanvas.query_courses(courses, opts.course))
-        num_matches = len(matched_courses)
 
-        if num_matches == 1:
-            self.current_course = matched_courses[0]
-        elif num_matches > 1:
-            self.poutput('Ambiguous input "{:s}".'.format(opts.course))
-            self.poutput('Please select an option:')
-
-            pad_length = len(str(num_matches)) + 2
-            format_str = '{:<' + str(pad_length) + '}{}'
-
-            self.poutput(format_str.format('0)', 'cancel'))
-            count = 1
-            for course in matched_courses:
-                self.poutput(format_str.format(f'{count})', course.unique_course_code))
-                count += 1
-
-            choice = input('Enter number: ')
-            if choice.isdigit():
-                num_choice = int(choice)
-                if num_choice > num_matches:
-                    self.poutput(f'Choice {num_choice} greater than last choice ({num_matches}).')
-                elif num_choice != 0:
-                    self.current_course = matched_courses[num_choice - 1]
-            else:
-                self.poutput(f'Choice {choice} is not numeric.')
-        else:
-            self.poutput('Could not find a matching course.')
+        match = utils.get_course_by_query(courses, opts.course)
+        if match is not None:
+            self.current_course = match
 
     def complete_cc(self, text, line, begidx, endidx):
         query = line[3:].replace(' ', '').lower()
         courses = self.get_courses()
 
-        return [course.unique_course_code for course in Clanvas.query_courses(courses, query)]
+        return [utils.unique_course_code(course) for course in utils.filter_courses(courses, query)]
 
     whoami_parser = argparse.ArgumentParser()
     whoami_parser.add_argument('-v', '--verbose', action='store_true', help='display more info about the logged in user')
---
+++ clanvas/utils.py
@@ -0,0 +1,111 @@
+import functools
+
+from tabulate import tabulate
+from tzlocal import get_localzone
+
+
+def cached_invalidatable(f):
+    cached_value = None
+
+    @functools.wraps(f)
+    def check_recalculate(self, **kwargs):
+        nonlocal cached_value
+        if kwargs.get('invalidate', False) or cached_value is None:
+            cached_value = f(self, **kwargs)
+        return cached_value
+
+    return check_recalculate
+
+
+def argparser_course_optional(argparser):
+    argparser.add_argument('-c', '--course', help='course id or matching course string (e.g. the course code)')
+    return argparser
+
+
+def argparser_course_optional_wrapper(with_argparser):
+    """
+    When applied to a do_x function in the Clanvas class that takes in argparser opts,
+    will convert/replace course attribute with a corresponding course object either
+    using the course string as a query or the current (cc'd) course if not supplied.
+    :param with_argparser: the cmd2.with_argparser method to be wrapped.
+    :return:
+    """
+    @functools.wraps(with_argparser)
+    def inject_argparser(self, opts):
+        if opts.course is not None:
+            opts.course = get_course_by_query(self, opts.course)
+        elif self.current_course is not None:
+            opts.course = self.current_course
+
+        with_argparser(self, opts)
+
+    return inject_argparser
+
+
+def unique_course_code(course):
+    return course.course_code.replace(' ', '') + '-' + str(course.id)
+
+
+def course_info_items(c):
+    return [c.course_code, c.id, c.start_at_date.strftime("%b %y") if hasattr(c, 'start_at_date') else '', c.name]
+
+
+def assignment_info_items(a):
+    return [a.id, a.due_at_date.astimezone(get_localzone()).strftime("%a, %d %b %I:%M%p") if hasattr(a, 'due_at_date') else '', a.name]
+
+
+def submission_info_items(s):
+    return [s.id, s.score if hasattr(s, 'score') else '']
+
+
+def course_query_items(c):
+    return [c.course_code, c.id, unique_course_code(c), c.name]
+
+
+def filter_courses(courses, query):
+    return filter(lambda course: any([query in str(item) for item in course_query_items(course)]), courses)
+
+
+def tabulate_dict(item_to_list, items):
+    return dict(zip(items, tabulate(map(item_to_list, items), tablefmt='plain').split('\n')))
+
+
+def get_course_by_query(clanvas, query, fail_on_ambiguous=False, quiet=False):
+    matched_courses = list(filter_courses(clanvas.get_courses(), query))
+    num_matches = len(matched_courses)
+
+    if num_matches == 1:
+        return matched_courses[0]
+    elif num_matches > 1 and not fail_on_ambiguous:
+        if not quiet:
+            clanvas.poutput('Ambiguous course input "{:s}".'.format(query))
+        if not quiet:
+            clanvas.poutput('Please select an option:')
+
+        pad_length = len(str(num_matches)) + 2
+        format_str = '{:<' + str(pad_length) + '}{}'
+
+        if not quiet:
+            clanvas.poutput(format_str.format('0)', 'cancel'))
+        count = 1
+        for course in matched_courses:
+            if not quiet:
+                clanvas.poutput(format_str.format(f'{count})', unique_course_code(course)))
+            count += 1
+
+        choice = input('Enter number: ')
+        if choice.isdigit():
+            num_choice = int(choice)
+            if num_choice > num_matches:
+                if not quiet:
+                    clanvas.poutput(f'Choice {num_choice} greater than last choice ({num_matches}).')
+            elif num_choice != 0:
+                return matched_courses[num_choice - 1]
+        else:
+            if not quiet:
+                clanvas.poutput(f'Choice {choice} is not numeric.')
+    else:
+        if not quiet:
+            clanvas.poutput('Could not find a matching course.' if not fail_on_ambiguous else 'Ambiguous course query string.')
+
+    return None
