1c779614f819b56b6651c80b7e7b9c0007f34f1d
Mark Lalor <markwlalor@gmail.com>
2018-03-11 17:53:09 -0400

    Finish initial setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,19 @@ setup(
     name='clanvas',
     version='0.1.0',
     url='https://github.com/MarkLalor/Clanvas',
-    license='LICENSE',
+    license='MIT',
     author='Mark Lalor',
     author_email='markwlalor@gmail.com',
     install_requires=['canvasapi', 'cmd2', 'tabulate', 'colorama', 'pytz', 'tzlocal', 'tree-format'],
     packages=['clanvas'],
     entry_points={'console_scripts': ['clanvas = clanvas.clanvas:main']},
-    description='Command-line client for Canvas by Instructure '
+    description='Command-line client for Canvas by Instructure',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Education',
+        'Topic :: System :: Shells',
+        'License :: OSI Approved :: MIT License'
+    ]
 )
